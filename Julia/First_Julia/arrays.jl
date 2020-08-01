fe4892d4aed3248d2a729a37268eacc179ab4049
spegla Nikola Macesic <spegla@gmail.com>
2020-08-02 00:20:42 +0200

    commit

---
+++ Julia/First_Julia/arrays.jl
@@ -0,0 +1,39 @@
+#basics
+
+#Int64 type
+a1 =[1,2,3,4,5]
+
+#Float64 type
+a2 =[1,2.0,3,4,5]
+
+#String type
+s1 = ["I","love","Julia","YEA"]
+
+#Function type
+f1 = [print,println,printstyled]
+
+#any type
+a3 = [1,2.0,"Julia"]
+
+#define type of arrey Int64
+type_int = Int64[1,2,3,4,5]
+
+#define type of arrey float
+type_float = Float64[1,2,3,4,5]
+
+#define type of arrey string
+type_string = String["My","name","is","Julia"]
+
+#2d two dimensional arrays
+array_2d = [1 2 3 4;5 6 7 8]
+
+#vector line with coma and without 
+vector_1 = [1 2 3 4 5]
+
+#random array
+array_rand = rand(3)
+array_rand = rand(5)
+
+#random 3 x rows and 3 coloms
+array_rand2 = rand(2,3)
+array_rand2 = rand(3,3)
