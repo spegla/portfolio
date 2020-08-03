9c51aba5a8b33f1bc9d0b7406d29a57999d65791
spegla Nikola Macesic <spegla@gmail.com>
2020-08-04 00:15:41 +0200

    commit

--- PythonBot/AutomateShopTitan.py
+++ PythonBot/AutomateShopTitan.py
@@ -1,37 +1,65 @@
 import pyautogui
 #import pydirectinput
 import webbrowser   
-import time
-import sys
+import time, sys, random
 
-#pydirectinput.moveTo(100, 150)
-#pyautogui.mouseDown(1000,735)
-#pyautogui.click(x=1008, y=742)
-time.sleep(1)
-######## finish crafted item #############
-#finish crafted item
-for PickupCraft in range(0,10):
-    pyautogui.click(x=1008, y=742)
-    time.sleep(1)
-    #Collect clik if have
-    pyautogui.click(478,636)
-    #time.sleep(1)
-##########################################    
 
-######## sell & surcharge item #############
-#seller first contact
-pyautogui.click(x=698,y=235)
-time.sleep(1)
-for sell in range(0,7):
-    #small talk
-    pyautogui.click(x=359,y=506)
-    #surcharge
-    pyautogui.click(x=782,y=520)
+
+for all in range(0,5):
+    
+    #pydirectinput.moveTo(100, 150)
+    #pyautogui.mouseDown(1000,735)
+    #pyautogui.click(x=1008, y=742)
     time.sleep(1)
-    #sell/buy
-    pyautogui.click(x=760,y=570)
-    #time.sleep(1)
-##########################################
-#pyautogui.click(x=1000, y=735)
-Position = pyautogui.position()
-print(Position)
+    ######## finish crafted item #############
+    #finish crafted item
+    for PickupCraft in range(0,10):
+        pyautogui.click(x=1008, y=742)
+        time.sleep(1)
+        #Collect click if have
+        pyautogui.click(478,636)
+        #new stuff confirmation
+        pyautogui.click(x=580,y=786)
+        #time.sleep(1)
+    ##########################################    
+
+    ######## sell & surcharge item #############
+    #seller first contact
+    #pyautogui.click(x=698,y=235)
+    pyautogui.click(x=674,y=201)
+    time.sleep(1)
+    for sell in range(0,7):
+        #small talk
+        pyautogui.click(x=359,y=506)
+        #suggest
+        pyautogui.click(x=405,y=443)
+        time.sleep(1)
+        #surcharge
+        pyautogui.click(x=782,y=520)
+        time.sleep(1)
+        #sell/buy
+        pyautogui.click(x=760,y=570)
+        #
+        
+        #time.sleep(1)
+    ##########################################
+    #Random & if statement
+    RandomCraft.random(1,1,10)
+    if RandomCraft = 
+    ######## Craft new item  #############
+    for craft in range(0,5):
+        # craft 
+        #odaberem craft
+        pyautogui.click(x=1076,y=738)
+        time.sleep(1)
+        #odaberem bow
+        pyautogui.click(x=236,y=805)
+        time.sleep(1)
+        #5 time click for crafting
+        pyautogui.click(x=434,y=682)
+        
+    ##########################################
+    #blanko
+    pyautogui.click(x=871,y=550)
+    time.sleep(70)
+    
--- PythonBot/recognizetext.py
+++ PythonBot/recognizetext.py
@@ -4,11 +4,15 @@ import sys
 import time
 #/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png
 import pyautogui
+import random
 
-button7location = pyautogui.locateOnScreen('googlelogo_color_272x92dp.png', confidence=0.9)
-button7location
+#button7location = pyautogui.locateOnScreen('googlelogo_color_272x92dp.png', confidence=0.9)
+#button7location
 #Box(left=1416, top=562, width=50, height=41)
 
 #pyautogui.locateOnScreen('googlelogo_color_272x92dp.png', region=(0,0, 300, 400))
 
 #button7location = pyautogui.locateOnScreen('googlelogo_color_272x92dp.png')
+r = random.randint(1,10)
+RandomCraft = random.randint(1,10)
+print(RandomCraft)
