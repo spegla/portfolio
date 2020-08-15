c575ec8e664fe24655fd886c9449ce3f502decd3
spegla Nikola Macesic <spegla@gmail.com>
2020-08-15 23:46:54 +0200

    Niko

--- PythonBot/EVEOnline/automining.py
+++ PythonBot/EVEOnline/automining.py
@@ -19,12 +19,13 @@ select_minigbelt_worpto0 = [964,230]
 #exit undock from space station
 pyautogui.moveTo(Undock_from_station[0],Undock_from_station[1],2)
 pyautogui.click()
-time.sleep(15)
+
+time.sleep(random.randint(15,20))
 #go to mining  post
 pyautogui.rightClick(select_mining_belt[0],select_mining_belt[1],2)
 pyautogui.moveTo(select_minigbelt_worpto0[0],select_minigbelt_worpto0[1],2)
 pyautogui.click()
-time.sleep(10)
+time.sleep(65)R
 #mining asteroid belt
 minig_asteroid1 = [966,242]
 minig_asteroid2 = [966,262]
@@ -37,18 +38,18 @@ pyautogui.hotkey('Shift', 'f')
 pyautogui.hotkey('Alt', 'F2')
 pyautogui.hotkey('Alt', 'F3')
 #MINING FIRST ASTEROID
-pyautogui.doubleClick(minig_asteroid1[0],minig_asteroid1[1],2)
+pyautogui.moveTo(minig_asteroid1[0],minig_asteroid1[1],2)
+pyautogui.doubleClick()
 time.sleep(3)
-pyautogui.moveTo(lock_target[0],lock_target[1]2)
+pyautogui.moveTo(lock_target[0],lock_target[1],2)
 time.sleep(1)
 pyautogui.hotkey('F1')
 #MINING SECOUND ASTEROID
 pyautogui.doubleClick(minig_asteroid2[0],minig_asteroid2[1],2)
 time.sleep(3)
-pyautogui.moveTo(lock_target[0],lock_target[1]2)
+pyautogui.moveTo(lock_target[0],lock_target[1],2)
 time.sleep(1)
 pyautogui.hotkey('F2')
-
 #this is a time to full fill cargo 5000m3
 #time.sleep(2479)
 time.sleep(60)
@@ -57,7 +58,7 @@ time.sleep(60)
 pyautogui.hotkey('Shift', 'r')
 time.sleep(1)
     #Dock to station
-pyautogui.rightkey(230,296)
+pyautogui.rightclick(230,296)
 pyautogui.moveTo(262,304,2)
 pyautogui.click()
 
