81b08b04f748259142c744e438c78667e523dac5
spegla Nikola Macesic <spegla@gmail.com>
2020-08-15 23:14:52 +0200

    Niko

--- PythonBot/EVEOnline/automining.py
+++ PythonBot/EVEOnline/automining.py
@@ -31,11 +31,34 @@ minig_asteroid2 = [966,262]
 minig_asteroid3 = [966,282]
 start_shield_hardener = [772,766]
 start_afterburner = [824,763]
+lock_target = [1023,83]
 #get drones out
 pyautogui.hotkey('Shift', 'f')
 pyautogui.hotkey('Alt', 'F2')
 pyautogui.hotkey('Alt', 'F3')
+#MINING FIRST ASTEROID
 pyautogui.doubleClick(minig_asteroid1[0],minig_asteroid1[1],2)
 time.sleep(3)
+pyautogui.moveTo(lock_target[0],lock_target[1]2)
+time.sleep(1)
+pyautogui.hotkey('F1')
+#MINING SECOUND ASTEROID
+pyautogui.doubleClick(minig_asteroid2[0],minig_asteroid2[1],2)
+time.sleep(3)
+pyautogui.moveTo(lock_target[0],lock_target[1]2)
+time.sleep(1)
+pyautogui.hotkey('F2')
+
+#this is a time to full fill cargo 5000m3
+#time.sleep(2479)
+time.sleep(60)
+#GO HOME
+    #drow back your drones
+pyautogui.hotkey('Shift', 'r')
+time.sleep(1)
+    #Dock to station
+pyautogui.rightkey(230,296)
+pyautogui.moveTo(262,304,2)
+pyautogui.click()
 
 
